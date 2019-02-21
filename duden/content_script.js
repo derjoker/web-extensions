30b11f56fbb4b3c563bf87c14066d7bd6b26e290
Feng Ya <fengya04@126.com>
2019-02-21 22:17:01 +0800

    semicolons

--- duden/content_script.js
+++ duden/content_script.js
@@ -2,245 +2,245 @@
 
 /* global $ */
 
-const KEY_ID = 'kvb'
+const KEY_ID = 'kvb';
 
-$('input:checkbox').remove()
-$(`button.${KEY_ID}`).remove()
+$('input:checkbox').remove();
+$(`button.${KEY_ID}`).remove();
 
 // storage
-function storage () {
-  function get (key) {
-    const item = window.localStorage.getItem(key) || '[]'
-    return new Set(JSON.parse(item))
+function storage() {
+  function get(key) {
+    const item = window.localStorage.getItem(key) || '[]';
+    return new Set(JSON.parse(item));
   }
 
-  function set (key, item) {
-    window.localStorage.setItem(key, JSON.stringify(Array.from(item)))
+  function set(key, item) {
+    window.localStorage.setItem(key, JSON.stringify(Array.from(item)));
   }
 
-  function add (key, value) {
-    const item = get(key)
-    item.add(value)
-    set(key, item)
+  function add(key, value) {
+    const item = get(key);
+    item.add(value);
+    set(key, item);
   }
 
-  function remove (key, value) {
-    const item = get(key)
-    item.delete(value)
-    if (item.size) set(key, item)
-    else window.localStorage.removeItem(key)
+  function remove(key, value) {
+    const item = get(key);
+    item.delete(value);
+    if (item.size) set(key, item);
+    else window.localStorage.removeItem(key);
   }
 
-  function keys () {
-    const keys = []
+  function keys() {
+    const keys = [];
     for (let i = 0; i < window.localStorage.length; i++) {
-      const key = window.localStorage.key(i)
-      if (key.startsWith(KEY_ID)) keys.push(key)
+      const key = window.localStorage.key(i);
+      if (key.startsWith(KEY_ID)) keys.push(key);
     }
-    return keys
+    return keys;
   }
 
-  function items () {
+  function items() {
     return keys().map(key => ({
       key,
       value: JSON.parse(window.localStorage.getItem(key)).map(item =>
         JSON.parse(item)
-      )
-    }))
+      ),
+    }));
   }
 
-  function clear () {
+  function clear() {
     keys().forEach(key => {
-      window.localStorage.removeItem(key)
-    })
+      window.localStorage.removeItem(key);
+    });
   }
 
-  return { add, remove, items, clear }
+  return { add, remove, items, clear };
 }
 
 // const names = window.location.pathname.split('/')
 // const stem = names[names.length - 1]
-const stem = window.location.pathname.split('/').pop()
+const stem = window.location.pathname.split('/').pop();
 const word = $('section#block-system-main > h1')
   .text()
-  .replace(/\u00AD/g, '')
+  .replace(/\u00AD/g, '');
 
-const section = $('h2:contains("Bedeutungsübersicht")').parents('section')
+const section = $('h2:contains("Bedeutungsübersicht")').parents('section');
 
 section.find('div.entry').each((_, entry) => {
-  const clone = $(entry).clone()
-  clone.find('figure').remove()
-  clone.find('.term-section').remove()
-  const definition = clone.text().trim()
+  const clone = $(entry).clone();
+  clone.find('figure').remove();
+  clone.find('.term-section').remove();
+  const definition = clone.text().trim();
 
   $(entry)
     .find('.term-section')
     .each((_, el) => {
-      const b = $(el).find('h3:contains("Beispiel") + span')
+      const b = $(el).find('h3:contains("Beispiel") + span');
       b.prepend(
         $('<input type="checkbox" />').data('kvb', {
           definition,
-          example: b.text().trim()
+          example: b.text().trim(),
         })
-      )
+      );
 
       const w = $(el).find(
         'h3:contains("Wendungen, Redensarten, Sprichwörter") + span'
-      )
-      const clone = w.parent().clone()
-      clone.find('h3').remove()
+      );
+      const clone = w.parent().clone();
+      clone.find('h3').remove();
       w.prepend(
         $('<input type="checkbox" />').data('kvb', {
           definition,
-          example: clone.text().trim()
+          example: clone.text().trim(),
         })
-      )
+      );
 
       $(el)
         .find('ul > li')
         .each((_, li) => {
           const example = $(li)
             .text()
-            .trim()
+            .trim();
           $(li).prepend(
             $('<input type="checkbox" />').data('kvb', {
               definition,
-              example
+              example,
             })
-          )
-        })
-    })
-})
+          );
+        });
+    });
+});
 
 section.find('ol > li > a').each((_, a) => {
   const definition = $(a)
     .text()
-    .trim()
-  const href = $(a).attr('href')
+    .trim();
+  const href = $(a).attr('href');
   // $(href).prepend('<input type="checkbox" />')
   $(href)
     .find('.term-section')
     .each((_, el) => {
-      const b = $(el).find('h3:contains("Beispiel") + span')
+      const b = $(el).find('h3:contains("Beispiel") + span');
       b.prepend(
         $('<input type="checkbox" />').data('kvb', {
           definition,
-          example: b.text().trim()
+          example: b.text().trim(),
         })
-      )
+      );
 
       const w = $(el).find(
         'h3:contains("Wendungen, Redensarten, Sprichwörter") + span'
-      )
-      const clone = w.parent().clone()
-      clone.find('h3').remove()
+      );
+      const clone = w.parent().clone();
+      clone.find('h3').remove();
       w.prepend(
         $('<input type="checkbox" />').data('kvb', {
           definition,
-          example: clone.text().trim()
+          example: clone.text().trim(),
         })
-      )
+      );
 
       $(el)
         .find('ul > li')
         .each((_, li) => {
           const example = $(li)
             .text()
-            .trim()
+            .trim();
           $(li).prepend(
             $('<input type="checkbox" />').data('kvb', {
               definition,
-              example
+              example,
             })
-          )
-        })
-    })
-})
+          );
+        });
+    });
+});
 
-const s = storage()
+const s = storage();
 
 $('input:checkbox').click(event => {
-  const data = $(event.target).data('kvb')
-  const key = KEY_ID + '-' + stem
-  const value = JSON.stringify(Object.assign({ word }, data))
-  console.log(key, value)
-  if (event.target.checked) s.add(key, value)
-  else s.remove(key, value)
-})
+  const data = $(event.target).data('kvb');
+  const key = KEY_ID + '-' + stem;
+  const value = JSON.stringify(Object.assign({ word }, data));
+  console.log(key, value);
+  if (event.target.checked) s.add(key, value);
+  else s.remove(key, value);
+});
 
 const save = $(`<button class="${KEY_ID}">Save</button>`).click(() => {
   // console.log(s.items())
-  const items = [].concat(...s.items().map(item => item.value))
+  const items = [].concat(...s.items().map(item => item.value));
   // console.log(items)
-  const csv = new CsvWriter()
+  const csv = new CsvWriter();
   const encodedUri =
     'data:text/csv;charset=utf-8,' +
-    encodeURIComponent(csv.arrayToCSV(items.map(item => Object.values(item))))
+    encodeURIComponent(csv.arrayToCSV(items.map(item => Object.values(item))));
 
-  const w = window.open(null, 'CSV')
-  w.location.href = encodedUri
+  const w = window.open(null, 'CSV');
+  w.location.href = encodedUri;
 
   $('input:checked').each((_, el) => {
-    $(el).click()
-  })
-  s.clear()
-})
+    $(el).click();
+  });
+  s.clear();
+});
 
-$('body').prepend(save)
+$('body').prepend(save);
 
 // CSV Writer
-function CsvWriter (del, enc) {
-  this.del = del || ',' // CSV Delimiter
-  this.enc = enc || '"' // CSV Enclosure
+function CsvWriter(del, enc) {
+  this.del = del || ','; // CSV Delimiter
+  this.enc = enc || '"'; // CSV Enclosure
 
   // Convert Object to CSV column
-  this.escapeCol = function (col) {
+  this.escapeCol = function(col) {
     if (isNaN(col)) {
       // is not boolean or numeric
       if (!col) {
         // is null or undefined
-        col = ''
+        col = '';
       } else {
         // is string or object
-        col = String(col)
+        col = String(col);
         if (col.length > 0) {
           // use regex to test for del, enc, \r or \n
           // if(new RegExp( '[' + this.del + this.enc + '\r\n]' ).test(col)) {
 
           // escape inline enclosure
-          col = col.split(this.enc).join(this.enc + this.enc)
+          col = col.split(this.enc).join(this.enc + this.enc);
 
           // wrap with enclosure
-          col = this.enc + col + this.enc
+          col = this.enc + col + this.enc;
         }
       }
     }
-    return col
-  }
+    return col;
+  };
 
   // Convert an Array of columns into an escaped CSV row
-  this.arrayToRow = function (arr) {
-    var arr2 = arr.slice(0)
+  this.arrayToRow = function(arr) {
+    var arr2 = arr.slice(0);
 
-    var i
+    var i;
 
-    var ii = arr2.length
+    var ii = arr2.length;
     for (i = 0; i < ii; i++) {
-      arr2[i] = this.escapeCol(arr2[i])
+      arr2[i] = this.escapeCol(arr2[i]);
     }
-    return arr2.join(this.del)
-  }
+    return arr2.join(this.del);
+  };
 
   // Convert a two-dimensional Array into an escaped multi-row CSV
-  this.arrayToCSV = function (arr) {
-    var arr2 = arr.slice(0)
+  this.arrayToCSV = function(arr) {
+    var arr2 = arr.slice(0);
 
-    var i
+    var i;
 
-    var ii = arr2.length
+    var ii = arr2.length;
     for (i = 0; i < ii; i++) {
-      arr2[i] = this.arrayToRow(arr2[i])
+      arr2[i] = this.arrayToRow(arr2[i]);
     }
-    return arr2.join('\r\n')
-  }
+    return arr2.join('\r\n');
+  };
 }
